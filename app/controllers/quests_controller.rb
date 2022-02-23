5af811da10c60b5b33ae2fc14763805491e68b78
chiku3 <p314chiku.y@gmail.com>
2022-02-23 04:46:14 +0000

    UPDATE:クエストタスクの分類

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -3,7 +3,6 @@ class QuestsController < ApplicationController
     @user = current_user
     @quest = Quest.new
     @quest.quest_tasks.build
-    @questions = Question.all
   end
 
   def create
@@ -15,89 +14,118 @@ class QuestsController < ApplicationController
     # elsif
       @quest.save
 
+      # カレンダー表示用タスクを保存
+          quest_task = QuestTask.new
+            quest_task.task_id = 1
+            quest_task.quest_id  = @quest.id
+            quest_task.start_time = @quest.created_at  # クエスト開始日
+          quest_task.save
+          quest_task = QuestTask.new
+            quest_task.task_id = 2
+            quest_task.quest_id  = @quest.id
+            quest_task.start_time = @quest.due_day.ago(30.days)  # 引越し３０日前
+          quest_task.save
+          quest_task = QuestTask.new
+            quest_task.task_id = 3
+            quest_task.quest_id  = @quest.id
+            quest_task.start_time = @quest.due_day.ago(14.days)  # 引越し１４日前
+          quest_task.save
+          quest_task = QuestTask.new
+            quest_task.task_id = 4
+            quest_task.quest_id  = @quest.id
+            quest_task.start_time = @quest.due_day  # 引越し当日
+          quest_task.save
+          quest_task = QuestTask.new
+            quest_task.task_id = 5
+            quest_task.quest_id  = @quest.id
+            quest_task.start_time = @quest.due_day.since(14.days)  # 引越し１４日後
+         quest_task.save
+
       # 必須のタスクを保存
-      task_0 = Task.where(id: [1,4,6,7,8,9,10,11,12,18,21,22,23,24,25,26,27,28,29,30,31,39,40])
+      task_0 = Task.where(id: [6,9,11,13,14,15,21,24,27,28,29,30,31,32,33,34,35,36,44,45])
         task_0.each do |task|
           quest_task = QuestTask.new
             quest_task.quest_id  = @quest.id
             quest_task.task_id = task.id
-            quest_task.start_time = @quest.due_day
           quest_task.save
         end
 
         if params[:quest][:question_4] == "1"      # 質問４で「賃貸」を選んだ場合に保存
-          task_4 = Task.where(id: [2,15,16,17])
+          task_4 = Task.where(id: [7,16,17,18,25,26])
           task_4.each do |task|
             quest_task = QuestTask.new
               quest_task.quest_id  = @quest.id
               quest_task.task_id = task.id
-              quest_task.start_time = @quest.due_day
             quest_task.save
           end
         end
 
         if params[:quest][:question_5] == "1"    # 質問５で「持ってる」を選んだ場合に保存
-          task_5 = Task.where(id: [36])
+          task_5 = Task.where(id: [37])
           task_5.each do |task|
             quest_task = QuestTask.new
               quest_task.quest_id  = @quest.id
               quest_task.task_id = task.id
-              quest_task.start_time = @quest.due_day
             quest_task.save
           end
         end
 
         if params[:quest][:question_6] == "1"    # 質問６で「持ってる」を選んだ場合に保存
-          task_6 = Task.where(id: [3,35,37,38])
+          task_6 = Task.where(id: [8,38,39,40])
           task_6.each do |task|
             quest_task = QuestTask.new
               quest_task.quest_id  = @quest.id
               quest_task.task_id = task.id
-              quest_task.start_time = @quest.due_day
             quest_task.save
           end
         end
 
         if params[:quest][:question_7] == "1"    # 質問７で「持ってる」を選んだ場合に保存
-          task_7 = Task.where(id: [13,34])
+          task_7 = Task.where(id: [20,41])
           task_7.each do |task|
             quest_task = QuestTask.new
               quest_task.quest_id  = @quest.id
               quest_task.task_id = task.id
-              quest_task.start_time = @quest.due_day
             quest_task.save
           end
         end
 
         if params[:quest][:question_8] == "1"    # 質問８で「一緒」を選んだ場合に保存
-          task_8 = Task.where(id: [5,14,32,33])
+          task_8 = Task.where(id: [10,19,42,43])
           task_8.each do |task|
             quest_task = QuestTask.new
               quest_task.quest_id  = @quest.id
               quest_task.task_id = task.id
-              quest_task.start_time = @quest.due_day
             quest_task.save
           end
         end
 
-        if params[:quest][:question_9] == "1"    # 質問９で「ある」を選んだ場合に保存
-          task_9 = Task.where(id: [19])
+        if params[:quest][:question_9] == "1"    # 質問９で「してる」を選んだ場合に保存
+          task_9 = Task.where(id: [12])
           task_9.each do |task|
             quest_task = QuestTask.new
               quest_task.quest_id  = @quest.id
               quest_task.task_id = task.id
-              quest_task.start_time = @quest.due_day
             quest_task.save
           end
         end
 
-        if params[:quest][:question_10] == "1"    # 質問１０で「とってる」を選んだ場合に保存
-          task_10 = Task.where(id: [20])
+        if params[:quest][:question_10] == "1"    # 質問１０で「ある」を選んだ場合に保存
+          task_10 = Task.where(id: [22])
           task_10.each do |task|
             quest_task = QuestTask.new
               quest_task.quest_id  = @quest.id
               quest_task.task_id = task.id
-              quest_task.start_time = @quest.due_day
+            quest_task.save
+          end
+        end
+
+        if params[:quest][:question_11] == "1"    # 質問11で「とってる」を選んだ場合に保存
+          task_11 = Task.where(id: [23])
+          task_11.each do |task|
+            quest_task = QuestTask.new
+              quest_task.quest_id  = @quest.id
+              quest_task.task_id = task.id
             quest_task.save
           end
         end
@@ -110,8 +138,13 @@ class QuestsController < ApplicationController
 
   def show
     @quest = Quest.find(params[:id])
-    @quest_task = QuestTask.where(quest_id: @quest.id)
-    @quest_tasks = QuestTask.where(quest_id: @quest.id)
+    @quest_task = QuestTask.where(quest_id: @quest.id).where("id > ?", 5)     #すべての実行タスク
+    @quest_tasks = QuestTask.where(quest_id: @quest.id).where("id < ?", 6)    #カレンダー表示用
+    @quest_task_1 = QuestTask.where(quest_id: @quest.id).where(task_id: [6..10])   #すぐやろうタスク
+    @quest_task_2 = QuestTask.where(quest_id: @quest.id).where(task_id: [11,12])   #引越し３０日前頃にやろうタスク
+    @quest_task_3 = QuestTask.where(quest_id: @quest.id).where(task_id: [13..24])   #引越し１４日前頃にやろうタスク
+    @quest_task_4 = QuestTask.where(quest_id: @quest.id).where(task_id: [25..28])   #引越し当日にやろうタスク
+    @quest_task_5 = QuestTask.where(quest_id: @quest.id).where(task_id: [29..45])   #引越し後１４日以内にやろうタスク
   end
 
   # def edit
@@ -174,7 +207,7 @@ class QuestsController < ApplicationController
     params.require(:quest).permit(:user_id, :due_day, :start_pref,
     :start_city, :goal_pref, :goal_city, :is_clear)
   end
-  
+
   def quest_task_params
     params.require(:quest_task).permit(:quest_id, :task_id, :is_clear)
   end
