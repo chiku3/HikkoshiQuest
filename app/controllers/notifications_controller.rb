5bbb1b3be43140b4d77178b84404422554dbf699
chiku3 <p314chiku.y@gmail.com>
2022-03-07 06:05:31 +0000

    UPDATE:レイアウト

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,11 @@
 class NotificationsController < ApplicationController
+
   def index
     @notifications = current_user.passive_notifications
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
-    end
+      @notifications.where(checked: false).each do |notification|
+        notification.update_attributes(checked: true)
+      end
+    @notification = @notifications.where.not(visitor_id: current_user.id)
   end
 
   private
