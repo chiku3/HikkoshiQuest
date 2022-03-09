5d76f9c05ff234d05f9d4228d8f394a0d3ce8f89
chiku3 <p314chiku.y@gmail.com>
2022-03-09 14:30:08 +0000

    UPDATE:ログイン機能

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,9 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  #def new
-  # super
-  #end
+  def new
+   redirect_to root_path
+  end
 
   # POST /resource/sign_in
   #def create
@@ -24,4 +24,14 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def fail
+    redirect_to root_path
+  end
+
+  protected
+
+  def auth_options
+    { scope: resource_name, recall: "#{controller_path}#fail" }
+  end
 end
