0e890c135819ca219b3a49b4bf937de28d7c4293
chiku3 <p314chiku.y@gmail.com>
2022-03-11 08:42:01 +0000

    UPDATE:都道府県選択

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,35 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+jQuery(document).on('turbolinks:load', function(){
+$.getJSON('/pref_city.json', function(data) {
+  
+    for(var i=1; i<48; i++) {
+      var code = ('00'+code).slice(-2); // ゼロパディング
+      
+      var num;
+      if (i.toString().length == 1) {
+        num = "0" + i.toString();
+      }else{
+        num =  i.toString();
+      }
+      
+      $('#select-pref').append('<option value="'+num+'">'+data[i-1][num].pref+'</option>');
+    }
+  });
+
+$('#select-pref').on('change', function() {
+  $('#select-city option:nth-child(n+2)').remove(); // ※1 市区町村フォームクリア
+    var select_pref = ('00'+$('#select-pref option:selected').val()).slice(-2);
+   
+    var key = Number(select_pref)-1;
+    $.getJSON('/pref_city.json', function(data) {
+      for(var i=0; i<data[key][select_pref].city.length; i++){
+        
+        $('#select-city').append('<option value="'+data[key][select_pref].city[i].id+'">'+data[key][select_pref].city[i].name+'</option>');
+      }
+  });
+});
+
+});
